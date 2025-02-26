db3e91530d5de963bc4e0ae7fb06464f0feed051
mantas petrovas <mantas10@gmail.com>
2025-02-26 15:34:23 +0100

    Forgot to add LaneFactory so here it is

---
+++ BowlingHallManagement/Factories/LaneFactory.cs
@@ -0,0 +1,51 @@
+﻿using BowlingHallManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingHallManagement.Factories
+{
+    /*Factory Pattern Implementation for Lane class*/
+    public class LaneFactory
+    {
+        /// <summary>
+        /// Creates a new Lane with the specified lane number
+        /// </summary>
+        /// <param name="laneNumber">The number to assign to the lane</param>
+        /// <returns>A new Lane object</returns>
+        public static Lane CreateLane(int laneNumber)
+        {
+            // Validate lane number
+            if (laneNumber <= 0)
+            {
+                throw new ArgumentException("Lane number must be positive", nameof(laneNumber));
+            }
+            
+            // Create the lane
+            return new Lane(laneNumber);
+        }
+        
+        /// <summary>
+        /// Creates a batch of sequential lanes starting from lane 1
+        /// </summary>
+        /// <param name="count">Number of lanes to create</param>
+        /// <returns>An array of Lane objects</returns>
+        public static Lane[] CreateLanes(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("Lane count must be positive", nameof(count));
+            }
+            
+            Lane[] lanes = new Lane[count];
+            for (int i = 0; i < count; i++)
+            {
+                lanes[i] = CreateLane(i + 1); // Lane numbers start at 1
+            }
+            
+            return lanes;
+        }
+    }
+}
