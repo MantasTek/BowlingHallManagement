e29479ca48a6629b65ce79826b8c494d655dbd33
mantas petrovas <mantas10@gmail.com>
2025-02-26 11:43:21 +0100

    Add project files.

---
+++ BowlingHallManagement/Models/Member.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingHallManagement.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime MemberSince { get; set; }
+
+        public Member(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            MemberSince = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Email: {Email}, MemberSince: {MemberSince.ToShortDateString()}";
+        }
+
+    }
+}
---
+++ BowlingHallManagement/UI/ConsoleUI(for interaction with user).cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingHallManagement.UI
+{
+    class ConsoleUI_for_interaction_with_user_
+    {
+    }
+}
